771dd722e31429fc07d783d312576e32ddb8169b
Travi995 <joseantoniotravieso259@gmail.com>
2025-04-24 15:28:41 -0400

    feat(db) connect to db postgre

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5433') ,
+      
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities:[],
+      synchronize: true,
+      // logging: true,
+    })
+  ],
   controllers: [],
   providers: [],
 })
