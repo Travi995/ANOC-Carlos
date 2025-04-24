9660b03d7de524f88a301b5a7d377a0aaaa6d558
Travi995 <joseantoniotravieso259@gmail.com>
2025-04-24 14:04:28 -0400

    feat(Global): add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe())
+  app.enableCors(false);
+
+  const config = new DocumentBuilder()
+    .setTitle('Chanoi Api')
+    .setDescription('API Description')
+    .setVersion('0.1')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
