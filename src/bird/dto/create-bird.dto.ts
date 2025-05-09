2c3e251867d0f9a3f41e16c2db580e935bea3647
Travi995 <joseantoniotravieso259@gmail.com>
2025-05-08 21:10:27 -0400

    dox(birdModule) dtos Comments

--- src/bird/dto/create-bird.dto.ts
+++ src/bird/dto/create-bird.dto.ts
@@ -1,20 +1,25 @@
 import { IsEnum, IsNotEmpty, IsString } from "class-validator"
 import { statusHEALTH } from "../entities/bird.entity"
+import { ApiProperty } from "@nestjs/swagger"
 
 export class CreateBirdDto {
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     ci:string
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     peso:string
 
+    @ApiProperty()
     @IsString()
     @IsEnum(statusHEALTH)
     statusSick:statusHEALTH
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     especie:string
