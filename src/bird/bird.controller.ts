74af599953acfcce5c560bae9d6549dd978b15bb
Travi995 <joseantoniotravieso259@gmail.com>
2025-05-08 21:13:42 -0400

    feat(birdModule): add Validation Rols

--- src/bird/bird.controller.ts
+++ src/bird/bird.controller.ts
@@ -2,7 +2,11 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from
 import { BirdService } from './bird.service';
 import { CreateBirdDto } from './dto/create-bird.dto';
 import { UpdateBirdDto } from './dto/update-bird.dto';
+import { AuthRole } from 'src/auth/decorator/authRole.decorator';
+import { ValidRoles } from 'src/auth/decorator/roleprotected.decorator';
 
+
+@AuthRole(ValidRoles.ADMIN,ValidRoles.ASOCIATE)
 @Controller('bird')
 export class BirdController {
   constructor(private readonly birdService: BirdService) {}
@@ -10,7 +14,7 @@ export class BirdController {
   @Post()
   create(@Body() createBirdDto: CreateBirdDto) {
     return this.birdService.create(createBirdDto);
-    
+
   }
 
   @Get()
